7e1aba9df2b5ed71f7a31b18faa527f4e6bd6e84
STEFANO LAZZARI <stefanolazzari79@libero.it>
2023-12-06 07:14:23 +0100

    firebase

--- src/components/BodyComponent.vue
+++ src/components/BodyComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <div class="sm:pl-2 sm:pr-2   lg:pl-60 lg:pr-60">
-      <h1 class="p-5 text-pink-600">{{this.$route.name}}</h1>
+      <h1 class="p-5">{{this.$route.name}}</h1>
         <div class="grid gap-2 sm:grid-cols-1 lg:grid-cols-1">
          <div class="p-6 bg-black/70 text-yellow-300 text-center items-center rounded-md">
             <p><slot /></p>
