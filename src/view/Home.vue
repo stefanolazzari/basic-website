25fae99487ae96e518ddaf08607c02cdc6de4ec2
STEFANO LAZZARI <stefanolazzari79@libero.it>
2023-12-14 17:34:27 +0100

    src path fix, title

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -4,7 +4,7 @@
 
 <template>
   <BodyComponent>
-    <!-- <img src="src/assets/20220129_221200_2.jpg" alt="" width="200" style="float:left; margin:10px"> -->
+    <img src="/src/assets/20220129_221200_2.jpg" alt="" width="200" style="float:left; margin:10px">
     <p>    Sono un insegnante di tecnologie musicali e coding. Dal 2010 realizzo progetti originali per persone disabili con Openframework(c++) e Arduino(Processing).
     Dal 2022 collaboro con  la società Naitsmania che si occupa dello sviluppo di Single Page Application. <br> Utilizzo principalmente Vue.js, Node.js, Express, Firebase, Quasar. Autonomamente ho realizzato una videochat sperimentale rivolta alla Dad.
     <br>Ho una lunga esperienza di insegnamento/formazione soprattutto nella fascia 10-18 anni.</p>
