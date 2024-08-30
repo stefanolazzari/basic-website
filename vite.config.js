4c25e0b2d58dd8fc98460e67870d7338746e24e7
STEFANO LAZZARI <stefanolazzari79@libero.it>
2024-08-30 09:32:58 +0200

    previous name

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:"/website/",
+  base:"/basic-website/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
